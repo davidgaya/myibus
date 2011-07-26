459c93de9a895616d48b0601ab13edea9e971a37
David Gaya <david.gaya@assertio.es>
2011-07-26 22:29:22 +0200

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myibus::Application.config.secret_token = 'c50245be82a035da473a5b64c02b65e1a18dfd352c43b01eb2704e0a9c08cc73fe76ac1b8bb0c47a97414ae1d2b6fce2d10510e47237f902ab9e2f3a94892e37'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Myibus::Application.routes.draw do
+  resources :home, :only => :index
+  root :to => 'home#index'
+end
